df9a1b10c1ef831052b3cd69af399e1c24b9cdd7
Kevin Paul <kpaul@ucar.edu>
2022-05-24 13:19:12 -0600

    Keep image up forever

--- images/pbsserver/entrypoint.sh
+++ images/pbsserver/entrypoint.sh
@@ -7,4 +7,14 @@ sed -i "s/PBS_START_SCHED=0/PBS_START_SCHED=1/" /etc/pbs.conf
 sed -i "s/PBS_START_COMM=0/PBS_START_COMM=1/" /etc/pbs.conf
 sed -i "s/\$clienthost .*/\$clienthost $hostname/" /var/spool/pbs/mom_priv/config
 /etc/init.d/pbs start
-exec /bin/bash
+/opt/pbs/bin/qmgr -c "set server flatuid=true"
+/opt/pbs/bin/qmgr -c "set server acl_roots+=alpha@*"
+/opt/pbs/bin/qmgr -c "set server acl_roots+=beta@*"
+/opt/pbs/bin/qmgr -c "set server operators+=alpha@*"
+/opt/pbs/bin/qmgr -c "set server operators+=beta@*"
+while [ $# -gt 0 ]
+do
+    /opt/pbs/bin/qmgr -c "create node $1"
+    shift
+done
+exec sleep infinity
