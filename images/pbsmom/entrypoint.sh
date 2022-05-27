e7f10ea3b251505f2355c0dab4f0d880c87c563e
Kevin Paul <kpaul@ucar.edu>
2022-05-27 15:21:48 -0600

    Remove unneeded PBS server name change

--- images/pbsmom/entrypoint.sh
+++ images/pbsmom/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 /bin/sh /etc/entrypoint.sh
 hostname=$(hostname)
-sed -i "s/PBS_SERVER=.*/PBS_SERVER=$PBS_SERVER/" /etc/pbs.conf
 sed -i "s/PBS_START_MOM=0/PBS_START_MOM=1/" /etc/pbs.conf
 sed -i "s/\$clienthost .*/\$clienthost $hostname/" /var/spool/pbs/mom_priv/config
 /etc/init.d/pbs start
