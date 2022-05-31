317f6fa06b6bd3a2a4187c51f2ca2c06550db66d
Kevin Paul <kpaul@ucar.edu>
2022-05-31 16:25:14 -0600

    Copy base config

--- images/pbsmom/entrypoint.sh
+++ images/pbsmom/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 /bin/sh /etc/entrypoint.sh
 hostname=$(hostname)
-sed -i "s/PBS_SERVER=CHANGE/PBS_SERVER=${PBS_SERVER}/" /etc/pbs.conf
-sed -i "s/PBS_START_MOM=0/PBS_START_MOM=1/" /etc/pbs.conf
+sed -i "s/PBS_SERVER=CHANGE/PBS_SERVER=${hostname}/" /etc/pbs.conf
 sed -i "s/\$clienthost .*/\$clienthost $hostname/" /var/spool/pbs/mom_priv/config
 /etc/init.d/pbs start
 exec sleep infinity
