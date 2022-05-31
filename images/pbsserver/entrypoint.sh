2d48fbf7e06b390f37d9c1e3c779c0cae551e518
Kevin Paul <kpaul@ucar.edu>
2022-05-31 16:27:41 -0600

    OpenPBS image

--- images/pbsserver/entrypoint.sh
+++ images/pbsserver/entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 hostname=$(hostname)
-sed -i "s/PBS_SERVER=.*/PBS_SERVER=$hostname/" /etc/pbs.conf
-sed -i "s/PBS_START_SERVER=0/PBS_START_SERVER=1/" /etc/pbs.conf
-sed -i "s/PBS_START_SCHED=0/PBS_START_SCHED=1/" /etc/pbs.conf
-sed -i "s/PBS_START_COMM=0/PBS_START_COMM=1/" /etc/pbs.conf
+sed -i "s/PBS_SERVER=CHANGE/PBS_SERVER=$hostname/" /etc/pbs.conf
 sed -i "s/\$clienthost .*/\$clienthost $hostname/" /var/spool/pbs/mom_priv/config
 /etc/init.d/pbs start
 /opt/pbs/bin/qmgr -c "set server flatuid=true"
 /opt/pbs/bin/qmgr -c "set server acl_roots+=alpha@*"
 /opt/pbs/bin/qmgr -c "set server acl_roots+=beta@*"
+/opt/pbs/bin/qmgr -c "set server acl_roots+=gamma@*"
 /opt/pbs/bin/qmgr -c "set server operators+=alpha@*"
 /opt/pbs/bin/qmgr -c "set server operators+=beta@*"
+/opt/pbs/bin/qmgr -c "set server operators+=gamma@*"
 while [ $# -gt 0 ]
 do
     /opt/pbs/bin/qmgr -c "create node $1"
