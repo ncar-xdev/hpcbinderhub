d7c37476aac92c54bd3e42dc99aa6d46f6043164
Kevin Paul <kpaul@ucar.edu>
2022-06-13 08:58:01 -0600

    Reduce log level

--- images/jupyterhub/jupyterhub_config.py
+++ images/jupyterhub/jupyterhub_config.py
@@ -5,7 +5,6 @@ import sshspawner
 import sudospawner
 import wrapspawner
 
-c.Application.log_level = 0
 c.JupyterHub.allow_named_servers = True
 c.JupyterHub.data_files_path = "/opt/jupyter/share/jupyterhub"
 c.JupyterHub.spawner_class = "wrapspawner.ProfilesSpawner"
@@ -28,9 +27,10 @@ c.ProfilesSpawner.profiles = [
 # --------------------------------
 c.SSHSpawner.remote_hosts = ["head"]
 c.SSHSpawner.remote_port_command = (
-    "/opt/jupyter/bin/python /opt/jupyter/bin/get_port.py --ip"
+    "/opt/jupyter/bin/python /opt/jupyter/bin/get_port.py"
 )
 c.SSHSpawner.ssh_keyfile = "/etc/ssh/client"
+c.SSHSpawner.hub_api_url = f"http://{c.JupyterHub.hub_connect_ip}:8081/hub/api"
 c.SSHSpawner.path = (
     "/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/pbs/bin:/opt/jupyter/bin"
 )
@@ -47,7 +47,6 @@ c.TorqueSpawner.batch_script = """#!/bin/bash -l
 #PBS -k eo
 #PBS -N jupyter-singleuser
 #PBS -V
-env
 conda activate
 {cmd}
 """
