1f57f93f1abfe56fd98f8963ec6bf9cb422ef165
Kevin Paul <kpaul@ucar.edu>
2022-06-24 14:52:27 -0600

    External Binder service now working w/o build/launch

--- images/jupyterhub/jupyterhub_config.py
+++ images/jupyterhub/jupyterhub_config.py
@@ -1,5 +1,7 @@
 # Configuration file for jupyterhub.
 # flake8: noqa
+import os
+
 import batchspawner
 import sshspawner
 import sudospawner
@@ -12,6 +14,33 @@ c.JupyterHub.hub_ip = "0.0.0.0"
 c.JupyterHub.hub_connect_ip = "jupyterhub.cluster.net"
 c.ConfigurableHTTPProxy.api_url = "http://jupyterhub.cluster.net:5432"
 c.Spawner.notebook_dir = "~/"
+
+# ---------------------------------------------
+# Register the BinderHub as an external service
+# ---------------------------------------------
+binder_client_id = os.getenv("BINDER_CLIENT_ID")
+if not binder_client_id:
+    raise ValueError("BINDER_CLIENT_ID not set")
+binder_api_token = os.getenv("BINDER_API_TOKEN")
+if not binder_api_token:
+    raise ValueError("BINDER_API_TOKEN not set")
+
+c.JupyterHub.services = [
+    {
+        "name": "binder",
+        "oauth_client_id": binder_client_id,
+        "api_token": binder_api_token,
+        "oauth_redirect_uri": "http://localhost:8585/oauth_callback",
+        "oauth_roles": ["user"],
+    },
+]
+c.JupyterHub.load_roles = [
+    {
+        "name": "user",
+        # grant all users access to all services
+        "scopes": ["access:services", "self"],
+    }
+]
 
 # --------------------------------
 # WrapSpawner Profiles/Options
